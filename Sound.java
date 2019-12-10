6467fad6153942c0143e56d7558596edfbc3be70
irsyadhani22 <58015078+irsyadhani22@users.noreply.github.com>
2019-12-10 12:05:04 +0700

    Menambahkan suara level up

--- Sound.java
+++ Sound.java
@@ -3,4 +3,5 @@ import java.applet.AudioClip;
 
 public class Sound {
 	public static final AudioClip EAT = Applet.newAudioClip(Sound.class.getResource("eat.wav"));
+	public static final AudioClip LEVELUP = Applet.newAudioClip(Sound.class.getResource("levelup.wav"));
 }
